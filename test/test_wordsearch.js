f1d776fa94d4bdb81adf8620ec52419209ed2389
Erica Sun <ericasun@seas.upenn.edu>
2020-06-04 01:39:37 +0000

    implemented

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,8 +1,6 @@
 const chai = require('chai');
 const assert = chai.assert;
-
 const wordSearch = require('../wordsearch.js')
-
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
@@ -16,10 +14,8 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'FRANK')
-
     assert.isFalse(result);
   });
-
   it("should return true if the word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -32,7 +28,48 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
-
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'S', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'O', 'P', 'F', 'E', 'L', 'D'],
+      ['Y', 'I', 'D', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'E', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'F', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CODE')
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present horizontally but backwards", function() {
+    const result = wordSearch([
+      ['A', 'S', 'E', 'D', 'O', 'C', 'A', 'L'],
+      ['S', 'E', 'O', 'P', 'F', 'E', 'L', 'D'],
+      ['Y', 'I', 'D', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'F', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'F', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CODE')
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present vertically but backwards", function() {
+    const result = wordSearch([
+      ['A', 'S', 'E', 'D', 'D', 'C', 'A', 'L'],
+      ['S', 'E', 'O', 'P', 'F', 'E', 'L', 'D'],
+      ['Y', 'I', 'D', 'F', 'Q', 'D', 'A', 'L'],
+      ['H', 'N', 'F', 'T', 'E', 'O', 'R', 'G'],
+      ['W', 'F', 'C', 'S', 'Y', 'C', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CODE')
     assert.isTrue(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const reversed = word.split('').reverse().join('');
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        let string = "";
+        letters.forEach(subArr => string += subArr[i]);
+        verticalJoin.push(string);
     }
+    // for (let i = 0; i < letters.length; i++) {
+    //     for (let j = 0; i < letters[i].length; i++) {
+    //         if (letters[i][j] === word.charAt(0)) {
+    //             let diagonalString = "";
+    //             for (let k = 0; k < word.length; k++) {
+    //                 if (letters[i + k][j + k] === word.charAt(k)) {
+    //                     diagonalString += word.charAt(k);
+    //                     console.log(diagonalString);
+    //                 }
+    //             }
+    //             if (diagonalString.length === word.length) {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    // }
+    for (const l of horizontalJoin) {
+        if (l.includes(word) || l.includes(reversed)) return true;
+    }
+    for (const l of verticalJoin) {
+        if (l.includes(word) || l.includes(reversed)) {
+            return true;
+        }
+    }
+    return false; 
 }
-
+// wordSearch([["a","b","c"], ["d", "e", "f"]], 'ae');
 module.exports = wordSearch
